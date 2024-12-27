59e1b5bcae5395aca635d9e582b25fe7ba51327f
AbelFiol <abelfiol2712@gmail.com>
2024-12-27 11:43:18 -0500

    Delete UrlLib2

--- UrlLib2.py
+++
@@ -1,6 +0,0 @@
-import urllib.request, urllib.parse, urllib.error
-
-file = urllib.request.urlopen('https://www.dr-chuck.com/page1.htm')  # Open the URL and retrieve the file.
-
-for line in file:
-    print(line.decode().strip())  # Decode bytes to string, strip whitespace, and print.
